4bca11bcbb7d560772d13b37eaa523c19744e20e
1Dennis Hosea <dennisodwaro@gmail.com>
2019-05-09 14:50:17 +0300

    changed the classes to id in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,5 @@
-$(document).ready(function() {
- $(".design" ).click(function(){
-   $(".our").toggle();
-
- });
- $(".devt").click(function() {
-   $(".hidden").toggle();
-   $(".showing").toggle();
- });
- $(".col-md-4").click(function() {
-   $(".showing").toggle();
-   $(".hidden").toggle();
- });
- $(".submit").click(function(event) {
-   event.PreventDefault()
-   console.log("clicked button")
-
-   var name = $(".name").val()
-   var email = $(".email").val()
-   var message = $(".message").val()
-   var status = $(".response")
-   statusElm.empty()
-
-   if(email.length > 5 && email.includes("@") && email.includes(".")) {
-     statusElm.append("<div>Email is valid</div>")
-   }else {
-     statusElm.append("<div>Email is not valid</div>")
-   }
-
-   if (name.length > 1) {
-     statusElm.append("<div>Name is valid</div>")
-   }else {
-     statusElm.append("<div>Name is not valid valid</div>")
-   }
-
-   if (message.length > :sunglasses: {
-     statusElm.append("<div>message is valid</div>")
-   }else {
-     statusElm.append("<div>message is not valid valid</div>")
-   }
- })
- $('.over').hover(function() {
-      $(this).animate({
-        opacity: '1'
-      });
-    },
-    function() {
-      $(this).animate({
-        opacity: '0'
-      });
-    });
+$(document).ready(function(){
+  $(".col-md-4").click(function(){
+    $(".par").slideToggle()
+  });
 });
